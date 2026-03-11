1240665e3fc4e42351c7f493ac69e053c7cef7b2
Carlos Hernandez <carlos_hdz57@hotmail.com>
2026-03-11 13:54:30 -0600

    cambio de diseño

--- src/styles/TableStyle.tsx
+++ src/styles/TableStyle.tsx
@@ -2,15 +2,21 @@
 import { CSSProperties } from "@mui/material";
 
 export const table_thead: CSSProperties = {
-    backgroundColor: '#003366',
+    backgroundColor: 'transparent',
     color: '#ffffff',
     textAlign: 'left',
+    fontSize: 38,
+    fontWeight: 'lighter',
 }
 
 export const table_cell_blue: CSSProperties = {
-    backgroundColor: '#003366',
     color: '#ffffff',
     textAlign: 'center',
+    borderRadius: 5, 
+    border: '2px solid #98E3FC',
+    backgroundColor: '#1B397D',
+    background: 'linear-gradient(0deg,rgba(27, 57, 125, .8) 0%, rgba(45, 82, 167, .5) 50%)',
+    transparent:0.5,
 }
 
 export const table_cell_blue_light: CSSProperties = {
@@ -23,8 +29,7 @@ export const table_padding: CSSProperties = {
 }
 
 export const table_tbody: CSSProperties = {
-    borderBottom: '1px solid #dddddd',
-    backgroundColor: '#f5f5f5',
+    backgroundColor: 'transparent',
 }
 
 export const table_ubicacion: CSSProperties = {
